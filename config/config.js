17a1161fd2403ef196a1ff1dce68ac338006406c
Aun Mohammad <aunmohammad254@gmail.com>
2025-08-30 03:19:44 +0500

    Config files

--- config/config.js
+++ config/config.js
@@ -1,5 +1,9 @@
+require("dotenv").config({
+  path: "./.env",
+});
+
 module.exports = {
   port: process.env.PORT || 3000,
-  mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/todoapp',
+  mongoURI: process.env.MONGO_URI || 'mongodb+srv://aunmohammad254_db_user:DZtAQGMkmnctzd1S@cluster0.xxothym.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
   jwtSecret: process.env.JWT_SECRET || 'supersecretjwtkey',
 };
