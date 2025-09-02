9ac0faad66c8270d0742cae71500ab4cbbeba8d6
Aun Mohammad <aunmohammad254@gmail.com>
2025-09-03 03:57:43 +0500

    Good Practices Implemented

--- app/src/models/user.js
+++ app/src/models/user.js
@@ -16,7 +16,7 @@ username: {
     required: [true, 'Email is required'],
     unique: true,
     lowercase: true,
-    match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email']
+    match: [/^[A-Za-z0-9]+([.-][A-Za-z0-9]+)*@[A-Za-z0-9]+([.-][A-Za-z0-9]+)*(\.[A-Za-z]{2,3})+$/, 'Please enter a valid email']
   },
   password: {
     type: String,
